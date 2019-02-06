d1c8d33a9165ccfe8133b8a0e95536c57ebe7f18
Rayhanshuvo <rayhanshuvo.bd@gmail.com>
2019-02-07 00:47:42 +0600

    Just trying from branch

--- src/main/java/com/bookstore/StudentController.java
+++ src/main/java/com/bookstore/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class StudentController {
 
+	//This is the controller
 	@RequestMapping("/")
 	public String home() {
 		
