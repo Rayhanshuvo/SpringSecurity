1e023d1e41a575a663f365985b9957ef9d98b6ea
Rayhanul Islam Shuvo <rayhanshuvo.bd@gmail.com>
2021-06-10 13:50:35 +0600

    Update StudentController.java
    rayhan commit

--- src/main/java/com/bookstore/StudentController.java
+++ src/main/java/com/bookstore/StudentController.java
@@ -3,12 +3,12 @@ package com.bookstore;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@RestController
 public class StudentController {
 	//Controller
 	@RequestMapping("/")
 	public String home() {
 	System.out.println();	
-		return "home.jsp";
+		return "rayhan";
 	}
 }
